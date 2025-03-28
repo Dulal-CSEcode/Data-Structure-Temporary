ba0b0efcad076c562036f55b5adefdb00944ae97
MD Dulal Hossain <dulal.md.cse@gmail.com>
2025-03-28 23:24:43 +0600

    Update sentinel_linear_search.c

--- sentinel_linear_search.c
+++ sentinel_linear_search.c
@@ -1,5 +1,5 @@
 /**
- * @file dulal 2
+ * @file dulal 3
  * @brief [Linear Search with Sentinel](https://en.wikipedia.org/wiki/Linear_search#With_a_sentinel) algorithm implementation
  * @details
  * This algorithm saves the last element of the array, 
